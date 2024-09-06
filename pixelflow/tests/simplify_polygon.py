b1ed469c2a5d555e4227a224ac552b54f30681b7
Emrah Nazif <emrah@ayofi.com>
2024-09-06 09:19:27 +0200

    calculate execution time of polygon simplify algorithms

--- pixelflow/tests/simplify_polygon.py
+++ pixelflow/tests/simplify_polygon.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot as plt
 from rdp import rdp
 from simplification.cutil import simplify_coords_vw
 from shapely.geometry import Polygon
+import time
 
 # Load your data from file
 file_path = './polygon.txt'
@@ -13,16 +14,28 @@ with open(file_path, 'r') as file:
 coordinates = [(data[i], data[i+1]) for i in range(0, len(data), 2)]
 
 # Apply RDP Algorithm
+# Measure execution time for RDP Algorithm
+start_rdp = time.time()
 rdp_simplified = rdp(coordinates, epsilon=2.0)
+end_rdp = time.time()
+rdp_time = end_rdp - start_rdp
 
 # Apply VW Algorithm
+# Measure execution time for VW Algorithm
+start_vw = time.time()
 vw_simplified = simplify_coords_vw(np.array(coordinates), 0.01)
 vw_simplified = [tuple(point) for point in vw_simplified]
+end_vw = time.time()
+vw_time = end_vw - start_vw
 
 # Apply Shapely Simplification
+# Measure execution time for Shapely Simplification
 polygon = Polygon(coordinates)
+start_shapely = time.time()
 shapely_simplified = polygon.simplify(tolerance=2.0, preserve_topology=True)
 shapely_simplified_coords = list(shapely_simplified.exterior.coords)
+end_shapely = time.time()
+shapely_time = end_shapely - start_shapely
 
 # Plot the Original and Simplified Polygons
 plt.figure(figsize=(15, 10))
@@ -56,6 +69,6 @@ plt.tight_layout()
 plt.show()
 
 # Print the number of points for all simplified polygons
-print(f'RDP Simplified Points: {len(rdp_simplified)}')
-print(f'VW Simplified Points: {len(vw_simplified)}')
-print(f'Shapely Simplified Points: {len(shapely_simplified_coords)}')
+print(f'RDP Simplified Points: {len(rdp_simplified)}, duration: {rdp_time}')
+print(f'VW Simplified Points: {len(vw_simplified)}, duration: {vw_time}')
+print(f'Shapely Simplified Points: {len(shapely_simplified_coords)}, duration: {shapely_time}')
