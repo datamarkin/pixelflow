bc40456337725273cdd67ee3a0a54fe197bc365b
Emrah Nazif <emrah@ayofi.com>
2025-08-27 14:02:37 +0200

    Expose `SlicedInference` and `Buffer` modules in PixelFlow's public API.

--- pixelflow/__init__.py
+++ pixelflow/__init__.py
@@ -9,12 +9,18 @@ from . import annotate
 from . import colors
 from . import zones
 from . import lines
+from . import slicer
+from . import buffer
 
 # Import specific functions for top-level access
 from .video import lazy_frame_generator
 from .zones import Zones, Zone
 from .lines import Lines, Line
+from .slicer import SlicedInference, auto_slice_size
+from .buffer import Buffer
 
 # Define the public API
-__all__ = ["draw", "annotate", "results", "zones", "lines", "lazy_frame_generator",
-           "Zones", "Zone", "Lines", "Line", "__version__", "__author__"]
+__all__ = ["draw", "annotate", "results", "zones", "lines", "slicer", "buffer", 
+           "lazy_frame_generator", "Zones", "Zone", "Lines", "Line", 
+           "SlicedInference", "auto_slice_size", "Buffer",
+           "__version__", "__author__"]
