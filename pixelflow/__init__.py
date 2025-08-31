63b671331d7b9ede470227d1381cee77a886e295
Emrah Nazif <emrah@ayofi.com>
2025-09-01 00:42:32 +0200

    Remove `draw` module and update public API to reflect changes.

--- pixelflow/__init__.py
+++ pixelflow/__init__.py
@@ -2,7 +2,6 @@ __version__ = "0.1.0"
 __author__ = "Datamarkin"
 
 # Import core modules
-from . import draw
 from . import results
 from . import video
 from . import annotators as annotate
@@ -20,7 +19,7 @@ from .slicer import SlicedInference, auto_slice_size
 from .buffer import Buffer
 
 # Define the public API
-__all__ = ["draw", "annotate", "results", "slicer", "buffer", 
+__all__ = ["annotate", "results", "slicer", "buffer",
            "lazy_frame_generator", "Zones", "Zone", "Lines", "Line", 
            "SlicedInference", "auto_slice_size", "Buffer",
            "__version__", "__author__"]
