dbaddb1fbcfbabb0a512cc8aadbb2418af9e1735
Emrah Nazif <emrah@ayofi.com>
2025-09-04 12:47:04 +0200

    Replace `lines` with `crossings` in imports and public API, and add `tracker` module to `__all__`.

--- pixelflow/__init__.py
+++ pixelflow/__init__.py
@@ -7,22 +7,23 @@ from . import video
 from . import annotators as annotate
 from . import colors
 from . import zones
-from . import lines
+from . import crossings
 from . import slicer
 from . import buffer
 from . import timer
+from . import tracker
 
 # Import specific functions for top-level access
 from .video import get_video_frames, VideoInfo, VideoWriter, Monitor
-from .zones import Zones, Zone
-from .lines import Lines, Line
+from .zones import Zones
+from .crossings import Crossings
 from .slicer import SlicedInference, auto_slice_size
 from .buffer import Buffer
 from .timer import TimeTracker
 
 # Define the public API
-__all__ = ["annotate", "detections", "slicer", "buffer", "timer", "video",
+__all__ = ["annotate", "detections", "slicer", "buffer", "timer", "video", "tracker",
            "get_video_frames", "VideoInfo", "VideoWriter", "Monitor",
-           "Zones", "Zone", "Lines", "Line", "TimeTracker",
+           "Zones", "Crossings", "TimeTracker",
            "SlicedInference", "auto_slice_size", "Buffer",
            "__version__", "__author__"]
