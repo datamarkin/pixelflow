2d4dea916aa9bf13aa526307194e96e8b09a8f61
Emrah Nazif <emrah@ayofi.com>
2025-09-04 03:01:32 +0200

    Revert "Add initial converters for unified Detections format, including Detectron2, Transformers, Ultralytics, and placeholders for MMDetection, SAM, and Datamarkin integrations."
    This reverts commit 62ceb8b5dd1a771b44fb1fe9bb6612579f6e6007.

--- pixelflow/converters/mmdetection.py
+++
@@ -1,9 +0,0 @@
-"""
-MMDetection results converter for PixelFlow
-
-Converts MMDetection detection results to PixelFlow's unified Detections format.
-"""
-
-# TODO: Implement MMDetection results conversion
-def from_mmdetection(mmdet_results):
-    return mmdet_results
--- pixelflow/converters/sam.py
+++
@@ -1,9 +0,0 @@
-"""
-SAM (Segment Anything Model) results converter for PixelFlow
-
-Converts SAM detection results to PixelFlow's unified Detections format.
-"""
-
-# TODO: Implement SAM results conversion
-def from_sam(sam_results):
-    return sam_results
